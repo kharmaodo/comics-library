3af7662b999da1f7b9af2ed967a666fe939cc04b
Maodo DIOP <maodo.diop@atos.net>
2017-09-26 14:25:50 +0000

    feat : Delete action, button to handle Delete action , stylesheet

--- src/ui/components/comics-library/component.ts
+++ src/ui/components/comics-library/component.ts
@@ -1,11 +1,16 @@
-import Component from '@glimmer/component';
+import Component, {tracked} from '@glimmer/component';
 
 export default class ComicsLibrary extends Component {
 //Ajout une liste de comics
 
-  comics = [
+  @tracked  comics = [
     {id: 1, title: "Akira", scriptwriter: "Katsuhiro Otomo", illustrator: "Katsuhiro Otomo"},
     {id: 2, title: "BlackSad", scriptwriter: "Juan Diaz Canales", illustrator: "Juanjo Guarnido"},
-    {id: 3, title: "Calvin and Hobbes", scriptwriter: "Bill Watterson", illustrator: "Bill Watterson"}
+    {id: 3, title: "Calvin and Hobbes", scriptwriter: "Bill Watterson", illustrator: "Bill Watterson"},
+    {id: 4, title: "Le mandat", scriptwriter: "Sembene Ousmane", illustrator: "Sembene Ousmane"}
   ];
+  //Ajout Action Supprimer
+  deleteComic(comic){
+    this.comics= this.comics.filter(x =>x.id!= comic.id);
+  }
 }
