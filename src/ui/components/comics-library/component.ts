4c9f952351d0c2cd849dc01b508b503ae127f28e
Maodo DIOP <maodo.diop@atos.net>
2017-09-26 14:00:34 +0000

    feat: list of comics

--- src/ui/components/comics-library/component.ts
+++ src/ui/components/comics-library/component.ts
@@ -1,5 +1,11 @@
 import Component from '@glimmer/component';
 
 export default class ComicsLibrary extends Component {
+//Ajout une liste de comics
 
+  comics = [
+    {id: 1, title: "Akira", scriptwriter: "Katsuhiro Otomo", illustrator: "Katsuhiro Otomo"},
+    {id: 2, title: "BlackSad", scriptwriter: "Juan Diaz Canales", illustrator: "Juanjo Guarnido"},
+    {id: 3, title: "Calvin and Hobbes", scriptwriter: "Bill Watterson", illustrator: "Bill Watterson"}
+  ];
 }
